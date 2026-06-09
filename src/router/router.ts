34af82ae0d954c90f590b6facc871b2eb2191fbf
xanedesu <xanedesu@gmail.com>
2026-06-09 20:47:40 +0300

    refactor: streamline router group handling and improve error response logic

--- src/router/router.ts
+++ src/router/router.ts
@@ -1,5 +1,5 @@
 import Router from 'find-my-way';
-import type { IncomingHttpHeaders, IncomingMessage } from 'node:http';
+import type { IncomingMessage } from 'node:http';
 import { Readable } from 'node:stream';
 import { pipeline } from 'node:stream/promises';
 import type { Config } from '../config/types.js';
@@ -27,6 +27,11 @@ interface HttpHandler {
   ): Promise<Response>;
 }
 
+export interface RouterGroup {
+  route(method: HttpMethod, path: string, handler: HttpHandler): void;
+  group(prefix: string, callback: (router: RouterGroup) => void): void;
+}
+
 export function createRouter(config: Config, logger: Logger) {
   const router = Router({
     defaultRoute(req, res) {
@@ -40,62 +45,89 @@ export function createRouter(config: Config, logger: Logger) {
     }
   });
 
+  const createRouterGroup = (prefix = '') => {
+    return {
+      route(method: HttpMethod, path: string, handler: HttpHandler) {
+        router.on(
+          method,
+          normalizePath(prefix, path),
+          async (req, res, params, _, searchParams) => {
+            try {
+              const result = await handler(
+                new Request(
+                  new URL(
+                    String(req.url),
+                    `http://localhost:${config.HTTP_PORT}`
+                  ),
+                  {
+                    duplex: 'half',
+                    method,
+                    headers: transformHeaders(req),
+                    body: extractBody(req)
+                  }
+                ),
+                params,
+                new URLSearchParams(searchParams)
+              );
+
+              res.writeHead(result.status, Object.fromEntries(result.headers));
+
+              if (result.body) {
+                await pipeline(Readable.fromWeb(result.body), res);
+
+                return res;
+              }
+
+              return res.end();
+            } catch (err) {
+              switch (true) {
+                case err instanceof HttpError:
+                  return res
+                    .writeHead(err.status, {
+                      'content-type': 'application/problem+json'
+                    })
+                    .end(JSON.stringify(err.withInstance(req.url)));
+              }
+
+              logger.error(String(err));
+
+              {
+                const err = new InternalServerError(
+                  undefined,
+                  undefined,
+                  req.url
+                );
+
+                return res
+                  .writeHead(err.status, {
+                    'content-type': 'application/problem+json'
+                  })
+                  .end(JSON.stringify(err));
+              }
+            }
+          }
+        );
+      },
+      group(path: string, callback: (router: RouterGroup) => void) {
+        callback(createRouterGroup(normalizePath(prefix, path)));
+      }
+    };
+  };
+
   return {
     lookup: router.lookup.bind(router),
-    route(method: HttpMethod, path: string, handler: HttpHandler) {
-      router.on(method, path, async (req, res, params, _, searchParams) => {
-        try {
-          const result = await handler(
-            new Request(
-              new URL(String(req.url), `http://localhost:${config.HTTP_PORT}`),
-              {
-                duplex: 'half',
-                method,
-                headers: transformHeaders(req.headers),
-                body: extractBody(req)
-              }
-            ),
-            params,
-            new URLSearchParams(searchParams)
-          );
-
-          res.writeHead(result.status, Object.fromEntries(result.headers));
-
-          if (result.body) {
-            await pipeline(Readable.fromWeb(result.body), res);
-
-            return res;
-          }
-
-          return res.end();
-        } catch (err) {
-          switch (true) {
-            case err instanceof HttpError:
-              return res
-                .writeHead(err.status, {
-                  'content-type': 'application/problem+json'
-                })
-                  .end(JSON.stringify(err.withInstance(req.url)));
-          }
-
-          logger.error(String(err));
-
-          {
-            const err = new InternalServerError(undefined, undefined, req.url);
-
-            return res
-              .writeHead(err.status, {
-                'content-type': 'application/problem+json'
-              })
-              .end(JSON.stringify(err));
-          }
-        }
-      });
-    }
+    ...createRouterGroup()
   };
 }
 
-function extractBody(req: IncomingMessage): Readable | null {
+function normalizePath(...paths: string[]) {
+  return `/${paths
+    .flatMap((path) => path.split('/'))
+    .filter(Boolean)
+    .join('/')}`;
+}
+
+function extractBody(req: IncomingMessage) {
   if (
     req.headers['content-length'] !== undefined ||
     req.headers['transfer-encoding'] !== undefined
@@ -106,16 +138,15 @@ function extractBody(req: IncomingMessage): Readable | null {
   return null;
 }
 
-function transformHeaders(headers: IncomingHttpHeaders): Headers {
-  return Object.entries(headers).reduce((acc, [key, val]) => {
-    if (val == null) {
-      return acc;
-    }
-
-    if (Array.isArray(val)) {
-      val.forEach((val) => acc.append(key, val));
-    } else {
-      acc.append(key, val);
+function transformHeaders(req: IncomingMessage) {
+  return Object.entries(req.headers).reduce((acc, [key, val]) => {
+    switch (true) {
+      case typeof val === 'string':
+        acc.append(key, val);
+        break;
+      case Array.isArray(val):
+        val.forEach((val) => acc.append(key, val));
+        break;
     }
 
     return acc;
