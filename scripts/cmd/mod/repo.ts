964138093ecffcd4592ecb76f220feba5395451c
xanedesu <xanedesu@gmail.com>
2026-06-19 20:10:47 +0300

    refactor: inline repo command main function for improved readability

--- scripts/cmd/mod/repo.ts
+++ scripts/cmd/mod/repo.ts
@@ -5,23 +5,21 @@ import { isRecord } from './common/helpers/record.ts';
 import { createRepositoryUrl, request, resolveRepository } from './common/helpers/repository.ts';
 import { withModuleTransaction } from './common/helpers/transaction.ts';
 
-export const repo: CmdMain = async ([subcommand, ...args], context) => {
-  switch (subcommand) {
-    case 'get':
-      await get(args, context);
-      return;
-    case 'set':
-      await set(args, context);
-      return;
-  }
-
-  throw new CmdError('Repo subcommand must be get or set');
-};
-
 registerCommand({
   name: 'repo',
   description: 'Get or set the module repository URL',
-  main: repo
+  async main([subcommand, ...args], context) {
+    switch (subcommand) {
+      case 'get':
+        await get(args, context);
+        return;
+      case 'set':
+        await set(args, context);
+        return;
+    }
+
+    throw new CmdError('Repo subcommand must be get or set');
+  }
 });
 
 const get: CmdMain = async (args, env) => {
